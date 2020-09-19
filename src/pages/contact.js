9f7ce8db6877947d33e6f5b2d5a2eb5c1b52cc2c
Sneha Singh <snehasingh.baj@gmail.com>
2020-09-19 21:49:28 +0530

    template cleaned

--- src/pages/contact.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Menu from 'components/Menu'
-import PageInfo from 'components/PageInfo'
-
-ReactDOM.render(<Menu />, document.getElementById('menu'))
-ReactDOM.render(<PageInfo />, document.getElementById('pageInfo'))
